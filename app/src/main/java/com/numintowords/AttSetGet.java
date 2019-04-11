b4ca25257d403ba04f320c2b7a64beb87b2eab2e
Softwarica College Lab <softwarica@softwarica.com>
2019-04-11 16:40:17 +0545

    Done from Lab Computer

---
+++ app/src/main/java/com/numintowords/AttSetGet.java
@@ -0,0 +1,14 @@
+package com.numintowords;
+
+class AttSetGet {
+
+    public String getNumber() {
+        return numbmer;
+    }
+
+    public void setNumber(String number) {
+        this.numbmer = number;
+    }
+
+    private String numbmer;
+}
